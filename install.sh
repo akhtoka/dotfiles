79679f49a11370103e970b1a83e82fc5cc154c2b
tsumayoji <akihito2301@gmail.com>
2018-09-19 00:21:08 +0900

    コマンド修正

--- install.sh
+++ install.sh
@@ -5,18 +5,18 @@ deploy() {
     DOTPATH=~/.dotfiles
 
     # git が使えるなら git
-    if has "git"; then
+    if type "git" > /dev/null 2>&1; then
         git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
     # 使えない場合は curl か wget を使用する
-    elif has "curl" || has "wget"; then
+    elif type "curl" > /dev/null 2>&1 || type "wget" > /dev/null 2>&1; then
         tarball="https://github.com/tsumayoji/dotfiles/archive/master.tar.gz"
 
         # どっちかでダウンロードして，tar に流す
-        if has "curl"; then
+        if type "curl" > /dev/null 2>&1; then
             curl -L "$tarball"
 
-        elif has "wget"; then
+        elif type "wget" > /dev/null 2>&1; then
             wget -O - "$tarball"
 
         fi | tar xv -
@@ -25,12 +25,12 @@ deploy() {
         mv -f dotfiles-master "$DOTPATH"
 
     else
-        die "curl or wget required"
+        exit "curl or wget required"
     fi
 
     cd ~/.dotfiles
     if [ $? -ne 0 ]; then
-        die "not found: $DOTPATH"
+        exit "not found: $DOTPATH"
     fi
 
     # 移動できたらリンクを実行する
